d48781a8b551c40f3dd860f649fac9cd344c2b3e
MunafBadarpura <munafbadarpura11@gmail.com>
2024-07-30 07:36:03 +0530

    df

---
+++ A22_CONTEXT/src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState, lazy, Suspense } from 'react'
+import { CountContext } from './assets/context';
+import { useContext } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  return (
+    <>
+      <CountContext.Provider value={{count, setCount}}>
+      <Count/>
+      </CountContext.Provider>
+    </>
+  )
+}
+
+
+function Count(){
+  return (
+    <div>
+      <CountRender/>
+      <Button/>
+    </div>
+  )
+}
+
+function CountRender(){
+  const {count} = useContext(CountContext)
+  return(
+    <div>
+      {count}
+    </div>
+  )
+}
+
+function Button(){
+  const { count, setCount } = useContext(CountContext);
+  return(
+    <div>
+      <button onClick={() => setCount((preCount) => preCount+1 )}>Increase</button>
+      <button onClick={() => setCount((preCount) => preCount-1 )}>Decrease</button>
+    </div>
+  )
+}
+
+
+export default App
